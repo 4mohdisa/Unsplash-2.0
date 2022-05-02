352b3931d4b7f6c25f859bfd134afffb098e755d
Xcoder <84847782+4mohdisa@users.noreply.github.com>
2022-05-02 14:47:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center">
+      <div className="w-full mt-36 my-0 mx-auto max-w-md  py-32 text-center bg-gray-100">
+        <h1 className="text-4xl font-bold ">Let's create Unsplash Clone 🚀</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
