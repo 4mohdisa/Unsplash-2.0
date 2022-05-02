aa82fcebad86e58edaea53520916f9664690d561
Xcoder <84847782+4mohdisa@users.noreply.github.com>
2022-05-02 14:49:32 +0530

    done!

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="flex items-center justify-center">
       <div className="w-full mt-36 my-0 mx-auto max-w-md  py-32 text-center bg-gray-100">
-        <h1 className="text-4xl font-bold ">Let's create Unsplash Clone 🚀</h1>
+        <h1 className="text-4xl font-bold ">Let's create Unsplash Done!! 🚀</h1>
       </div>
     </div>
   );
